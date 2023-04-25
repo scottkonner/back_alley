35d4043d62ccdff182fbe2e248d0b7084e0ea32e
scottkonner <scott_konner@yahoo.com>
2023-04-24 21:01:11 -0400

    started work on 'game' routes

---
+++ app/api/game_routes.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, jsonify, session, request
+from flask_login import login_required, current_user
+from app.models import Game, User, db
+from datetime import datetime
+
+
+game_routes = Blueprint('game', __name__)
+
+##Get all Games
+@game_routes.route('/')
+@login_required
+def get_all_games():
+    """
+    Query for all games and return them in a list
+    """
+    games = Game.query.all()
+    return jsonify({'games': [game.to_dict() for game in games]})
+
+##Get Game by Id
+@game_routes.route('/<int:id>')
+@login_required
+def game_by_id(id):
+    """
+    Query for a game by id and returns that game in a dictionary
+    """
+    game = Game.query.get(id)
+    if game is None:
+        return jsonify({"error": "Game not found"}), 404
+    return game.to_dict()
+
+#Get all Games of Current User
+@game_routes.route('/current', methods=["GET"])
+@login_required
+def get_games_by_current_user():
+    games = Game.query.filter(
+        Game.user_id == current_user.id,
+        ).all()
+    return jsonify([game.to_dict() for game in games])
